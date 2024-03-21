ae5df3ea41b19a38149faa12e0195d7f3c61ddd3
devsecopsmurali <devsecopsmurali@gmail.com>
2024-03-21 11:16:48 +0530

    comment unwanted code

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
-variable "aws_access_key" {
-  description = "AWS access key ID"
-}
+# variable "aws_access_key" {
+#   description = "AWS access key ID"
+# }
 
-variable "aws_secret_key" {
-  description = "AWS secret access key"
-}
-variable "aws_region" {
-  description = "AWS region"
-}
+# variable "aws_secret_key" {
+#   description = "AWS secret access key"
+# }
+# variable "aws_region" {
+#   description = "AWS region"
+# }
 
 variable "aws_account_name" {
   description = "AWS account name"
