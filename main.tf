815d2303c11c2a14e4d77abe21285ab5ddc23f74
devsecopsmurali <devsecopsmurali@gmail.com>
2024-04-02 10:40:37 +0530

    role name added to main file

--- main.tf
+++ main.tf
@@ -2,4 +2,7 @@ resource "aws_organizations_account" "new_account" {
   name   = var.aws_account_name
   email  = var.aws_account_email
   #role_name        = aws_iam_role.cross_account_role.name
+  role_name = "myOrganizationRole"
+
+ 
 }
